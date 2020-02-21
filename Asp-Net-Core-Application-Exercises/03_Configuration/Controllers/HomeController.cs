11b715e025165ffc39786d67155e539a27e97546
Mateusz <mateusz@o2.pl>
2020-02-21 13:06:57 +0100

    Completed Custom Configuration project

---
+++ Asp-Net-Core-Application-Exercises/03_Configuration/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace _03_Configuration.Controllers
+{
+    public class HomeController : Controller
+    {
+       public IConfiguration Configuration { get; }
+
+        public HomeController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            CustomSettings settings = new CustomSettings();
+            Configuration.GetSection("CustomConfig").Bind(settings);
+
+            return Content($"CreatedDate: {settings.CreatedDate} \r\n" +
+                $"GitHubUrl: {settings.GitHubUrl}");
+        }
+    }
+}
---
+++ Asp-Net-Core-Application-Exercises/03_Configuration/CustomSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _03_Configuration
+{
+    public class CustomSettings
+    {
+        public string GitHubUrl { get; set; }
+        public string CreatedDate { get; set; }
+    }
+}
